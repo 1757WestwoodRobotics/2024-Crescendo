dfb33031b2ff019de398b08a3b09fca863e5ca04
LandonBayer <landonbayer0@gmail.com>
2024-03-16 18:03:45 -0400

    autos :)

--- commands/autospecific.py
+++ commands/autospecific.py
@@ -1,7 +1,4 @@
 from commands2.sequentialcommandgroup import SequentialCommandGroup
-from commands2.waitcommand import WaitCommand
-from commands.defensestate import DefenseState
-from commands.intakecommands import GroundIntake
 from commands.intakesetting import FeedIntakeToShooter, FloorIntake, HoldIntakeAtHandoff
 from commands.shooter.alignandaim import AlignAndAim
 from commands.shooter.shooterfixedshots import SafetyPosition
@@ -10,7 +7,6 @@ AimAndFire = lambda shooter, drive, intake: SequentialCommandGroup(
     HoldIntakeAtHandoff(intake),
     AlignAndAim(shooter, drive, (lambda: 0), (lambda: 0)),
     FeedIntakeToShooter(intake),
-    DefenseState(drive),
 )
 
 IntakeAuto = lambda intake, shooter: SequentialCommandGroup(
